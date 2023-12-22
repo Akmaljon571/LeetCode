a97852aa5c870b04f17c4788a67315f8fc8e18cf
Akmaljon571 <akmaljon079@gmail.com>
2023-12-22 20:40:05 +0300

    buildArray function

--- index.py
+++ index.py
@@ -1,7 +1,16 @@
+# class Solution:
+#     def defangIPaddr(self, address: str) -> str:
+#         return address.replace(".", "[.]")
+#
+#
+# test = Solution()
+# print(test.defangIPaddr(address="1.1.1.1"))
+
+
 class Solution:
-    def defangIPaddr(self, address: str) -> str:
-        return address.replace(".", "[.]")
+    def buildArray(self, nums):
+        return [nums[a] for a in nums]
 
 
 test = Solution()
-print(test.defangIPaddr(address="1.1.1.1"))
+print(test.buildArray(nums=[0, 2, 1, 5, 3, 4]))
