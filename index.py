6528f3fb11ccdd40e5c35eb051394607d3a7d210
Akmaljon571 <akmaljon079@gmail.com>
2023-12-22 20:52:45 +0300

    finalValueAfterOperations function

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+# 1108
 # class Solution:
 #     def defangIPaddr(self, address: str) -> str:
 #         return address.replace(".", "[.]")
@@ -7,10 +8,26 @@
 # print(test.defangIPaddr(address="1.1.1.1"))
 
 
+# 1920
+# class Solution:
+#     def buildArray(self, nums):
+#         return [nums[a] for a in nums]
+#
+#
+# test = Solution()
+# print(test.buildArray(nums=[0, 2, 1, 5, 3, 4]))
+
+# 2011
 class Solution:
-    def buildArray(self, nums):
-        return [nums[a] for a in nums]
+    def finalValueAfterOperations(self, operations) -> int:
+        b = 0
+        for a in operations:
+            if a == '--X' or a == 'X--':
+                b -= 1
+            elif a == '++X' or a == 'X++':
+                b += 1
+        return b
 
 
 test = Solution()
-print(test.buildArray(nums=[0, 2, 1, 5, 3, 4]))
+print(test.finalValueAfterOperations(operations = ["++X","++X","X++"]))
