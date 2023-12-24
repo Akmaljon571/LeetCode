d030da680b467cc6bf11ce0cbb35d7de82007b94
Akmaljon571 <akmaljon079@gmail.com>
2023-12-24 22:04:41 +0300

    2785 function

--- index.py
+++ index.py
@@ -1,8 +1,19 @@
 class Solution:
-    def entityParser(self, text):
-        return text.replace('&quot;', '"').replace('&gt;', '>').replace('&lt;', '<').replace('&apos;', "'").replace(
-            '&amp;', '&').replace('&frasl;', '/')
+    def sortVowels(self, s: str) -> str:
+        unli = 'aeiouAEIOU'
+        index = []
+        unli_arr = []
+        result = [*s]
+        for i, a in enumerate(s):
+            if a in unli:
+                unli_arr.append(a)
+                index.append(i)
+        unli_arr.sort()
+        for i, a in enumerate(unli_arr):
+            result[index[i]] = a
+        return ''.join(str(i) for i in result)
+
 
 
 test = Solution()
-print(test.entityParser(text="&amp; is an HTML entity but &ambassador; is not."))
+print(test.sortVowels(s = "lEetcOde"))
